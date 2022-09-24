ecd756e0834cc28b3fc5d3971ceb1d89dd02ad90
hirano-vm4 <hirano.vm4@gmail.com>
2022-09-24 22:11:28 +0900

    ボーリング課題の助言に対する助言の反映

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -5,24 +5,23 @@
 score = ARGV[0].gsub('X', '10,0').split(',').map(&:to_i)
 
 # 配列の中のスコアを2つずつにわける（10フレーム3投目はあまりの配列とした）
-scores = []
-score.each_slice(2) { |s| scores << s }
+scores = score.each_slice(2).to_a
 
 # フレームごとにわけた配列に番号をつけてハッシュ化
-hash = {}
-scores.each.with_index(1) { |s, i| hash.store(i, s) }
+flame_score = {}
+scores.each.with_index(1) { |s, i| flame_score.store(i, s) }
 
 # 得点の計算
 total = 0
 
-hash.each do |key, value|
+flame_score.each do |key, value|
   total +=
-    if value == [10, 0] && key <= 9 && hash.fetch(key + 1) == [10, 0] # 9フレームまでに連続ストライクした場合
-      value.sum + hash.fetch(key + 1)[0] + hash.fetch(key + 2)[0]
+    if value == [10, 0] && key <= 9 && flame_score.fetch(key + 1) == [10, 0] # 9フレームまでに連続ストライクした場合
+      value.sum + flame_score.fetch(key + 1)[0] + flame_score.fetch(key + 2)[0]
     elsif value == [10, 0] && key <= 9 # ストライクの場合
-      value.sum + hash.fetch(key + 1).sum
+      value.sum + flame_score.fetch(key + 1).sum
     elsif value.sum == 10 && key <= 9 # スペアの場合
-      value.sum + hash.fetch(key + 1)[0]
+      value.sum + flame_score.fetch(key + 1)[0]
     else
       value.sum
     end
