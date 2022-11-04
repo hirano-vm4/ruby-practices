cb2ee744e80c752e5e459edf2f0f56fae9293ce6
hirano-vm4 <hirano.vm4@gmail.com>
2022-11-04 12:12:56 +0900

    lsコマンド2の提出

--- 05.ls/myls.rb
+++ 05.ls/myls.rb
@@ -1,23 +1,25 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'optparse'
+
 DISPLAY_NUMBER = 3
 
+def main
+  acquired_elements = current_element
+  puts modified_element(acquired_elements)
+end
+
 def current_element
-  Dir.glob('*').sort
+  options = ARGV.getopts('a')
+  options['a'] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
 end
 
-def height
-  (current_element.size / DISPLAY_NUMBER) + 1
-end
-
-def string_count
-  current_element.map(&:length).max
-end
-
-def modified_element
-  sliced = current_element.map { |d| d.ljust(string_count) }.each_slice(height).to_a
+def modified_element(acquired_elements)
+  height = acquired_elements.size / DISPLAY_NUMBER + 1
+  string_count = acquired_elements.map(&:length).max
+  sliced = acquired_elements.map { |d| d.ljust(string_count) }.each_slice(height).to_a
   sliced.map { |data| data.values_at(0...height) }.transpose.map { |display| display.join(' ') }
 end
 
-puts modified_element
+main
