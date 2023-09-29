e82bfb0ee9ce383176628bc041d23800a13d0bba
hirano-vm4 <hirano.vm4@gmail.com>
2023-09-30 00:31:07 +0900

    OOP版ボーリングの課題提出

---
+++ 07.bowling_object/bowling_object.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require_relative 'game'
+
+def main
+  formated_argument = format_args(ARGV[0])
+  puts Game.new(formated_argument).score
+end
+
+  private
+
+def format_args(argument)
+  game_results = []
+  frame = []
+
+  argument.split(',').each do |score|
+    if game_results.length < 9
+      if score == 'X'
+        frame.push(score, 0)
+      else
+        frame.push(score)
+      end
+
+      if frame.length == 2
+        game_results.push(frame)
+        frame = []
+      end
+    else
+      frame.push(score)
+    end
+  end
+  game_results << frame
+end
+
+main
