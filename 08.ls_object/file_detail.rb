084ed81ef9c8a0012fa63c26eac75c29ffd3ea43
hirano-vm4 <hirano.vm4@gmail.com>
2023-10-19 09:50:23 +0900

    個別ファイルを操作するメソッドを定義・定数をクラス内に移動

--- 08.ls_object/file_detail.rb
+++ 08.ls_object/file_detail.rb
@@ -1,90 +1,62 @@
 # frozen_string_literal: true
 
-CONVERT_PERMISSION_VIEW = {
-  '0' => '---',
-  '1' => '--x',
-  '2' => '-w-',
-  '3' => '-wx',
-  '4' => 'r--',
-  '5' => 'r-x',
-  '6' => 'rw-',
-  '7' => 'rwx'
-}.transform_values(&:freeze).freeze
-
-FILE_TYPE = {
-  'file' => '-',
-  'directory' => 'd',
-  'link' => 'l'
-}.transform_values(&:freeze).freeze
-
 class FileDetail
-  def initialize(contents)
-    @stat_files = contents.map { |content| File.lstat(content) }
+  CONVERT_PERMISSION_VIEW = {
+    '0' => '---',
+    '1' => '--x',
+    '2' => '-w-',
+    '3' => '-wx',
+    '4' => 'r--',
+    '5' => 'r-x',
+    '6' => 'rw-',
+    '7' => 'rwx'
+  }.transform_values(&:freeze).freeze
+
+  FILE_TYPE = {
+    'file' => '-',
+    'directory' => 'd',
+    'link' => 'l'
+  }.transform_values(&:freeze).freeze
+
+  def initialize(content)
+    @name = content
+    @stat = File.lstat(content)
   end
 
-  def get_detail(contents)
-    total_block_size
-    necessary_elements = [
-      types_and_permissions,
-      hard_link,
-      owner,
-      group,
-      file_size,
-      last_update_time,
-      filename(contents)
-    ]
-    replaced_elements = necessary_elements.transpose
-    replaced_elements.map { |element| element.join(' ') }
+  def file_type
+    FILE_TYPE[@stat.ftype]
   end
 
-  private
-
-  def total_block_size
-    bs = 0
-    @stat_files.each { |file| bs += file.blocks }
-    puts "total #{bs}"
+  def permission
+    octal_mode = @stat.mode.to_s(8).slice(-3..-1).split('')
+    octal_mode.map { |octal| CONVERT_PERMISSION_VIEW[octal] }.join('')
   end
 
-  def file_types
-    @stat_files.map { |file| FILE_TYPE[file.ftype] }
-  end
-
-  def permissions
-    @stat_files.map do |file|
-      formatted_permissions = file.mode.to_s(8).slice(-3..-1).split('')
-      formatted_permissions.map { |converted_element| CONVERT_PERMISSION_VIEW[converted_element] }.join('')
-    end
-  end
-
-  def types_and_permissions
-    file_types.zip(permissions).map { |file| file.join('') }
+  def block_size
+    @stat.blocks
   end
 
   def hard_link
-    maximum_digit = @stat_files.map { |element| element.nlink.to_s.length }.max
-    @stat_files.map { |element| element.nlink.to_s.rjust(maximum_digit + 1) }
+    @stat.nlink.to_s
   end
 
   def owner
-    maximum_digit = @stat_files.map { |file| Etc.getpwuid(file.uid).name.length }.max
-    @stat_files.map { |file| Etc.getpwuid(file.uid).name.ljust(maximum_digit + 1) }
+    Etc.getpwuid(@stat.uid).name
   end
 
   def group
-    maximum_digit = @stat_files.map { |file| Etc.getgrgid(file.gid).name.length }.max
-    @stat_files.map { |file| Etc.getgrgid(file.gid).name.ljust(maximum_digit + 1) }
+    Etc.getgrgid(@stat.gid).name
   end
 
   def file_size
-    maximum_digit = @stat_files.map { |file| file.size.to_s.length }.max
-    @stat_files.map { |file| file.size.to_s.rjust(maximum_digit) }
+    @stat.size.to_s
   end
 
   def last_update_time
-    @stat_files.map { |file| file.mtime.strftime('%_m %_d %R') }
+    @stat.mtime.strftime('%_m %_d %R')
   end
 
-  def filename(contents)
-    contents.map { |content| FileTest.symlink?(content) ? content + " -> #{File.readlink(content)}" : content }
+  def file_name
+    File.symlink?(@name) ? @name + " -> #{File.readlink(@name)}" : @name
   end
 end
