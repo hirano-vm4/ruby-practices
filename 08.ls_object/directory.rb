6959044cd65ccdae79a78a8e36419d073e78a7bf
hirano-vm4 <hirano.vm4@gmail.com>
2023-10-28 19:16:29 +0900

    冗長な引数を減らすためにlオプションを表示するメソッドのロジックを変更

--- 08.ls_object/directory.rb
+++ 08.ls_object/directory.rb
@@ -38,53 +38,53 @@ class Directory
   end
 
   def display_content_details
-    stat_files = @dir_contents.map { |content| FileDetail.new(content) }
-    total_block_size(stat_files)
+    @stat_files = @dir_contents.map { |content| FileDetail.new(content) }
+    total_block_size
     [
-      types_and_permissions(stat_files),
-      hard_links(stat_files),
-      owners(stat_files),
-      groups(stat_files),
-      file_sizes(stat_files),
-      last_modification_time(stat_files),
-      display_names(stat_files)
+      types_and_permissions,
+      hard_links,
+      owners,
+      groups,
+      file_sizes,
+      last_modification_time,
+      display_names
     ].transpose.map { |element| element.join(' ') }
   end
 
-  def total_block_size(stat_files)
-    bs = stat_files.sum(&:block_size)
+  def total_block_size
+    bs = @stat_files.map(&:block_size).sum
     puts "total #{bs}"
   end
 
-  def types_and_permissions(stat_files)
-    stat_files.map(&:file_type).zip(stat_files.map(&:permission)).map(&:join)
+  def types_and_permissions
+    @stat_files.map(&:file_type).zip(@stat_files.map(&:permission)).map(&:join)
   end
 
-  def hard_links(stat_files)
-    max_length = stat_files.map { |file| file.hard_link.length }.max
-    stat_files.map { |file| file.hard_link.rjust(max_length + 1) }
+  def hard_links
+    max_length = @stat_files.map { |file| file.hard_link.length }.max
+    @stat_files.map { |file| file.hard_link.rjust(max_length + 1) }
   end
 
-  def owners(stat_files)
-    max_length = stat_files.map { |file| file.owner.length }.max
-    stat_files.map { |file| file.owner.ljust(max_length + 1) }
+  def owners
+    max_length = @stat_files.map { |file| file.owner.length }.max
+    @stat_files.map { |file| file.owner.ljust(max_length + 1) }
   end
 
-  def groups(stat_files)
-    max_length = stat_files.map { |file| file.group.length }.max
-    stat_files.map { |file| file.group.ljust(max_length + 1) }
+  def groups
+    max_length = @stat_files.map { |file| file.group.length }.max
+    @stat_files.map { |file| file.group.ljust(max_length + 1) }
   end
 
-  def file_sizes(stat_files)
-    max_length = stat_files.map { |file| file.file_size.length }.max
-    stat_files.map { |file| file.file_size.rjust(max_length) }
+  def file_sizes
+    max_length = @stat_files.map { |file| file.file_size.length }.max
+    @stat_files.map { |file| file.file_size.rjust(max_length) }
   end
 
-  def last_modification_time(stat_files)
-    stat_files.map(&:last_update_time)
+  def last_modification_time
+    @stat_files.map(&:last_update_time)
   end
 
-  def display_names(stat_files)
-    stat_files.map(&:file_name)
+  def display_names
+    @stat_files.map(&:file_name)
   end
 end
